1ce4d4066bda7617c00fe8b53518cc212115990a
vipdosantos <vipdosanto@gmail.com>
2021-09-28 12:35:27 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPragrama.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPragrama {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		 Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("HEllo world "+ (a+b));*/
+
+	}
+
+}
+
+class livros {
+	private String nome;
+	private String npag;
+}
